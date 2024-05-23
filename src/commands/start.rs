36183a2bebbf0a6f1d73611d6636c7737101c25c
Nozomi-Hijikata <nozomi.hijikata@techouse.jp>
2024-05-23 17:17:06 +0900

    wip

--- src/commands/start.rs
+++ src/commands/start.rs
@@ -24,6 +24,7 @@ impl Start {
         let candidate_dir_name = &self.name;
         let candidate_dir = Path::new("interviews").join(candidate_dir_name);
 
+        //
         create_directory(&candidate_dir);
 
         let template_path = Path::new("templates").join(&self.template);
